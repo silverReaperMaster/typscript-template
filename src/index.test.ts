301978c3ac9ea3e1a97f0ceb6d6f8e2f82442813
Kevin Genskowsky Jeanneret <kgenskowsky@gmail.com>
2022-06-16 22:28:23 -0400

    clean up

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,7 +1,6 @@
 import { expect, it } from 'vitest';
 import * as index from './index';
 
-// eslint-disable-next-line @typescript-eslint/no-empty-function
 it('index', () => {
     expect(index.indexOutput).toBeTypeOf('number');
 });
